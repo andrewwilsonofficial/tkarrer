7efeecc614ec19b26197725a06a54d626a06222f
andrew <al3almyibrahim@gmail.com>
2024-01-26 11:08:07 +0200

    Set iframe height

--- resources/views/public/view-report.blade.php
+++ resources/views/public/view-report.blade.php
@@ -3,7 +3,7 @@
 
 @section('content')
     <section id="iframe-section">
-        <div class="container">
+        <div class="container pt-4">
             <iframe src="https://docs.google.com/gview?url={{ asset('uploads/' . $report->file_path) }}&embedded=true"
                 style="width:100%; height:80vh;" frameborder="0"></iframe>
         </div>
