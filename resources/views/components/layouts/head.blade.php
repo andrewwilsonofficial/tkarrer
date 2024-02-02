507fa6797e8f98a48c5243931be0b68121643ea1
andrew <al3almyibrahim@gmail.com>
2024-02-02 18:30:31 +0200

    Google search console

--- resources/views/components/layouts/head.blade.php
+++ resources/views/components/layouts/head.blade.php
@@ -9,3 +9,12 @@
 <link rel="stylesheet" href="{{ asset('assets/css/landing.css') }}">
 <link rel="stylesheet" href="{{ asset('assets/css/bootstrap-datepicker.min.css') }}">
 <meta name="google-site-verification" content="D4euphY0tsZZ43OxEhKjKiSOERqNkXU74xZTMEIti3A" />
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-K3NBW6J1BT"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-K3NBW6J1BT');
+</script>
