a7a7f6f28a7e654f94a05c809ce607f433eb91e6
andrew <al3almyibrahim@gmail.com>
2024-02-18 08:18:14 +0200

    Fix error

--- resources/views/dashboard/reports/edit.blade.php
+++ resources/views/dashboard/reports/edit.blade.php
@@ -1,5 +1,9 @@
 @extends('layouts.dashboard-master')
 @section('title', 'تعديل التقرير')
+@section('head')
+    @parent
+    <link rel="stylesheet" href="{{ asset('assets/css/bootstrap-datepicker.min.css') }}">
+@endsection
 @section('content')
     <!-- Content -->
     <div class="container-xxl flex-grow-1 container-p-y">
@@ -32,10 +36,11 @@
                     </div>
                     <div class="mb-3">
                         <label for="published_at" class="form-label">
-                            المصدر
+                            العام
                         </label>
-                        <input type="date" class="form-control @error('published_at') is-invalid @enderror"
-                            id="published_at" name="published_at" value="{{ old('published_at', $report->published_at) }}" required>
+                        <input type="text" class="form-control published_at @error('published_at') is-invalid @enderror"
+                            id="published_at" name="published_at" value="{{ old('published_at', $report->published_at) }}"
+                            required>
                         @error('published_at')
                             <div class="invalid-feedback">{{ $message }}</div>
                         @enderror
@@ -97,4 +102,21 @@
         </div>
     </div>
     <!-- / Content -->
+@endsection
+
+@section('scripts')
+    @parent
+    <script src="{{ asset('assets/js/bootstrap-datepicker.min.js') }}?123"></script>
+    <script>
+        $('.published_at').datepicker({
+            format: 'yyyy',
+            viewMode: 'years',
+            minViewMode: 'years',
+            autoclose: true,
+            clearBtn: true,
+            todayHighlight: false,
+            endDate: new Date(),
+            language: 'ar',
+        });
+    </script>
 @endsection
